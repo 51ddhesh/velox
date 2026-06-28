e75d62e332bf7efe80d03d2b856ab191bafd8ed4
siddhesh <173941233+51ddhesh@users.noreply.github.com>
2026-06-28 14:43:37 +0530

    [core]: scaffold entry point

---
+++ src/velox.cpp
@@ -0,0 +1,13 @@
+// Copyright 2026 51ddhesh
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <velox/config.hpp>
+
+namespace velox {
+
+// init scaffold 
+// left empty for now
+
+} // namespace velox 
